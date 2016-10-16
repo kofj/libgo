ae707248ef729bea4fdf3fc4f2aefbcb978cc3b3
Frank Kung <kfanjian@gmail.com>
2016-10-16 18:22:32 +0800

    add examples for kcptun.

---
+++ kcptun/examples/client/client.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/urfave/cli"
+
+	kcptun "github.com/kofj/libgo/kcptun/client"
+)
+
+var (
+	// VERSION is injected by buildflags
+	VERSION = "SELFBUILD"
+)
+
+func main() {
+	rand.Seed(int64(time.Now().Nanosecond()))
+	if VERSION == "SELFBUILD" {
+		// add more log flags for debugging
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+	}
+	myApp := cli.NewApp()
+	myApp.Name = "kcptun"
+	myApp.Usage = "client(with SMUX)"
+	myApp.Version = VERSION
+	myApp.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "localaddr,l",
+			Value: ":12948",
+			Usage: "local listen address",
+		},
+		cli.StringFlag{
+			Name:  "remoteaddr, r",
+			Value: "vps:29900",
+			Usage: "kcp server address",
+		},
+		cli.StringFlag{
+			Name:   "key",
+			Value:  "it's a secrect",
+			Usage:  "pre-shared secret between client and server",
+			EnvVar: "KCPTUN_KEY",
+		},
+		cli.StringFlag{
+			Name:  "crypt",
+			Value: "aes",
+			Usage: "aes, aes-128, aes-192, salsa20, blowfish, twofish, cast5, 3des, tea, xtea, xor, none",
+		},
+		cli.StringFlag{
+			Name:  "mode",
+			Value: "fast",
+			Usage: "profiles: fast3, fast2, fast, normal",
+		},
+		cli.IntFlag{
+			Name:  "conn",
+			Value: 1,
+			Usage: "set num of UDP connections to server",
+		},
+		cli.IntFlag{
+			Name:  "autoexpire",
+			Value: 0,
+			Usage: "set auto expiration time(in seconds) for a single UDP connection, 0 to disable",
+		},
+		cli.IntFlag{
+			Name:  "mtu",
+			Value: 1350,
+			Usage: "set maximum transmission unit for UDP packets",
+		},
+		cli.IntFlag{
+			Name:  "sndwnd",
+			Value: 128,
+			Usage: "set send window size(num of packets)",
+		},
+		cli.IntFlag{
+			Name:  "rcvwnd",
+			Value: 1024,
+			Usage: "set receive window size(num of packets)",
+		},
+		cli.IntFlag{
+			Name:  "datashard",
+			Value: 10,
+			Usage: "set reed-solomon erasure coding - datashard",
+		},
+		cli.IntFlag{
+			Name:  "parityshard",
+			Value: 3,
+			Usage: "set reed-solomon erasure coding - parityshard",
+		},
+		cli.IntFlag{
+			Name:  "dscp",
+			Value: 0,
+			Usage: "set DSCP(6bit)",
+		},
+		cli.BoolFlag{
+			Name:  "nocomp",
+			Usage: "disable compression",
+		},
+		cli.BoolFlag{
+			Name:   "acknodelay",
+			Usage:  "flush ack immediately when a packet is received",
+			Hidden: true,
+		},
+		cli.IntFlag{
+			Name:   "nodelay",
+			Value:  0,
+			Hidden: true,
+		},
+		cli.IntFlag{
+			Name:   "interval",
+			Value:  40,
+			Hidden: true,
+		},
+		cli.IntFlag{
+			Name:   "resend",
+			Value:  0,
+			Hidden: true,
+		},
+		cli.IntFlag{
+			Name:   "nc",
+			Value:  0,
+			Hidden: true,
+		},
+		cli.IntFlag{
+			Name:   "sockbuf",
+			Value:  4194304, // socket buffer size in bytes
+			Hidden: true,
+		},
+		cli.IntFlag{
+			Name:   "keepalive",
+			Value:  10, // nat keepalive interval in seconds
+			Hidden: true,
+		},
+		cli.StringFlag{
+			Name:  "log",
+			Value: "",
+			Usage: "specify a log file to output, default goes to stderr",
+		},
+		cli.StringFlag{
+			Name:  "c",
+			Value: "", // when the value is not empty, the config path must exists
+			Usage: "config from json file, which will override the command from shell",
+		},
+	}
+	myApp.Action = func(c *cli.Context) error {
+		config := kcptun.Config{}
+		config.LocalAddr = c.String("localaddr")
+		config.RemoteAddr = c.String("remoteaddr")
+		config.Key = c.String("key")
+		config.Crypt = c.String("crypt")
+		config.Mode = c.String("mode")
+		config.Conn = c.Int("conn")
+		config.AutoExpire = c.Int("autoexpire")
+		config.MTU = c.Int("mtu")
+		config.SndWnd = c.Int("sndwnd")
+		config.RcvWnd = c.Int("rcvwnd")
+		config.DataShard = c.Int("datashard")
+		config.ParityShard = c.Int("parityshard")
+		config.DSCP = c.Int("dscp")
+		config.NoComp = c.Bool("nocomp")
+		config.AckNodelay = c.Bool("acknodelay")
+		config.NoDelay = c.Int("nodelay")
+		config.Interval = c.Int("interval")
+		config.Resend = c.Int("resend")
+		config.NoCongestion = c.Int("nc")
+		config.SockBuf = c.Int("sockbuf")
+		config.KeepAlive = c.Int("keepalive")
+		config.Log = c.String("log")
+
+		if c.String("c") != "" {
+			err := parseJSONConfig(&config, c.String("c"))
+			checkError(err)
+		}
+
+		// log redirect
+		if config.Log != "" {
+			f, err := os.OpenFile(config.Log, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+			checkError(err)
+			defer f.Close()
+			log.SetOutput(f)
+		}
+		kcptun.Start(config)
+
+		return nil
+	}
+	myApp.Run(os.Args)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Printf("%+v\n", err)
+		os.Exit(-1)
+	}
+}
+
+func parseJSONConfig(config *kcptun.Config, path string) error {
+	file, err := os.Open(path) // For read access.
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return json.NewDecoder(file).Decode(config)
+}
